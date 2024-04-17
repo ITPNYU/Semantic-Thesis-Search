bc8097190e93607de52f087668e64cc16d058201
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-04-16 22:57:21 -0400

    just changing to 2024 venue

--- embeddings-transformers.js
+++ embeddings-transformers.js
@@ -4,17 +4,20 @@ import { pipeline } from '@xenova/transformers';
 // Load the embeddings model
 const extractor = await pipeline('feature-extraction', 'Xenova/bge-small-en-v1.5');
 
-const venuesData = fs.readFileSync('venues.json', 'utf-8');
-const { venues } = JSON.parse(venuesData);
+// const venuesData = fs.readFileSync('venues.json', 'utf-8');
+// const { venues } = JSON.parse(venuesData);
+
+let venues = [{ name: '2024', id: 202 }];
 
 for (let venue of venues) {
   const outputJSON = { embeddings: [] };
   console.log(`Processing venue: ${venue.name}...`);
   const projectData = fs.readFileSync(`thesis-data/thesis-${venue.id}.json`, 'utf-8');
-  const projects = JSON.parse(projectData);
+  const { projects } = JSON.parse(projectData);
   for (let project of projects) {
-    // let text = project.project_name + '\n' + project.description;
-    let text = project.project_name + ': ' + project.elevator_pitch;
+    let title = project.project_name;
+    let student = project.users[0].user_name;
+    let text = title + '\n' + project.description + '\n' + student;
     let output;
     try {
       output = await extractor(text, {
@@ -25,7 +28,7 @@ for (let venue of venues) {
       console.log(e);
     }
     const embedding = output.tolist()[0];
-    outputJSON.embeddings.push({ text, embedding });
+    outputJSON.embeddings.push({ title, student, text, embedding });
   }
   const fileOut = `embeddings/thesis-embeddings-${venue.id}.json`;
   fs.writeFileSync(fileOut, JSON.stringify(outputJSON));
--- fetch-projects-db.js
+++ fetch-projects-db.js
@@ -1,10 +1,13 @@
 import fs from 'fs';
 
-let data = fs.readFileSync('venues.json', 'utf-8');
-let { venues } = JSON.parse(data);
+// let data = fs.readFileSync('venues.json', 'utf-8');
+// let { venues } = JSON.parse(data);
+
+let venues = [{ id: 202 }];
 
 for (let i = 0; i < venues.length; i++) {
   let venue = venues[i];
+  console.log(venue);
   let url = `https://itp.nyu.edu/projects/public/projectsJSON_ALL.php?venue_id=${venue.id}`;
 
   console.log(`Fetching data from ${url}...`);
